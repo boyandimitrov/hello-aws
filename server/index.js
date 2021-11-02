9546f927b158cb7903506decda945d0dd2f5f063
Boyan Dimitrov <boyan.dimitrov@gmail.com>
2021-11-02 17:02:35 +0200

    root

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.json({ message: "Solve AWS elastic beanstalk 100.0 % of the requests are erroring with HTTP 4xx" });
+});
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
